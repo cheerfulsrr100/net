a710ca33be6fff35efad1b74c04a748e0fc300c8
Green <cheerfuls@outlook.com>
2022-08-04 17:56:20 +0800

    fix close_wait

--- unp-rs/src/util.rs
+++ unp-rs/src/util.rs
@@ -10,7 +10,10 @@ pub fn str_echo(stream: &mut TcpStream) -> Result<()> {
     let mut buf = [0; 128];
     loop {
         if stream.read(&mut buf)? > 0 {
+            print!("{}",String::from_utf8_lossy(&buf));
             stream.write(&buf)?;
+        } else {
+            return Ok(());
         }
     }
 }
@@ -23,7 +26,7 @@ pub fn str_cli(stream: &mut TcpStream) -> Result<()> {
         if stdin().read_line(&mut input)? > 0 {
             stream.write(input.as_bytes())?;
             stream.read(&mut out)?;
-            println!("{}", String::from_utf8_lossy(&out));
+            print!("aaa {}", String::from_utf8_lossy(&out));
             input.clear();
             unsafe {
                 libc::memset(out.as_mut_ptr() as *mut c_void, 0, size_of::<[u8; 128]>());
